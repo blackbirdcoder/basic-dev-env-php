dea2f4a7bf7505128f487738be5aef0cf5699db3
Eugene-TechnoNinja <technoninja@ukr.net>
2022-07-25 15:43:49 +0300

    established interaction with the database, added env

--- src/index.php
+++ src/index.php
@@ -1,2 +1,12 @@
 <?php
-phpinfo();
+$host = getenv('APP_NAME') . '_' . getenv('CONTAINER_MYSQL');
+$user = getenv('MYSQL_USER');
+$pass = getenv('MYSQL_PASSWORD');
+
+$conn = new mysqli($host, $user, $pass);
+if ($conn->connect_error) {
+    die("Connect failed: " . $conn->connect_error);
+} else {
+    echo "Connect ti MySQL server successfully!";
+}
+
